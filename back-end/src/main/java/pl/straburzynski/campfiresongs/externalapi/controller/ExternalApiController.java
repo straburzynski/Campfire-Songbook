ecab66e7373feb5d1705363aad187860b9803047
Sebastian Straburzyński <sebastian@straburzynski.pl>
2021-09-04 21:43:29 +0200

    External API - update session.

--- back-end/src/main/java/pl/straburzynski/campfiresongs/externalapi/controller/ExternalApiController.java
+++ back-end/src/main/java/pl/straburzynski/campfiresongs/externalapi/controller/ExternalApiController.java
@@ -1,5 +1,6 @@
 package pl.straburzynski.campfiresongs.externalapi.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,23 +12,22 @@ import pl.straburzynski.campfiresongs.externalapi.model.ExternalApiSong;
 import pl.straburzynski.campfiresongs.externalapi.service.ExternalApiParserService;
 import pl.straburzynski.campfiresongs.externalapi.service.ExternalApiRestService;
 import pl.straburzynski.campfiresongs.externalapi.service.ExternalApiSongScrapperService;
+import pl.straburzynski.campfiresongs.model.SessionDto;
 import pl.straburzynski.campfiresongs.model.Song;
+import pl.straburzynski.campfiresongs.model.SongDto;
+import pl.straburzynski.campfiresongs.service.SessionService;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "external")
+@RequiredArgsConstructor
 public class ExternalApiController {
 
     private final ExternalApiRestService externalApiRestService;
     private final ExternalApiSongScrapperService scrapper;
-
-    public ExternalApiController(ExternalApiRestService externalApiRestService,
-                                 ExternalApiSongScrapperService scrapper) {
-        this.externalApiRestService = externalApiRestService;
-        this.scrapper = scrapper;
-    }
+    private final SessionService sessionService;
 
     @GetMapping("/search")
     public List<ExternalApiSong> searchByTitle(@RequestParam("title") String title) {
@@ -38,14 +38,29 @@ public class ExternalApiController {
     public Song parseSongFromUrl(@RequestBody ExternalApiSong externalApiSong) {
         Document document = scrapper.getDocumentByUrl(externalApiSong.getUrl());
         String lyrics = ExternalApiParserService.parseLyricsFromDocument(document);
-        Song song = Song.builder()
+        return Song.builder()
                 .id(UUID.randomUUID())
                 .lyrics(lyrics)
                 .title(externalApiSong.getTitle())
                 .author(externalApiSong.getArtist())
                 .build();
-        return song;
     }
 
+    @PostMapping("/updateSession")
+    public SessionDto updateSession(@RequestBody ExternalApiSong externalApiSong,
+                                    @RequestParam("sessionName") String sessionName) {
+        Document document = scrapper.getDocumentByUrl(externalApiSong.getUrl());
+        String lyrics = ExternalApiParserService.parseLyricsFromDocument(document);
+        SessionDto sessionDto = SessionDto.builder()
+                .name(sessionName)
+                .temporary(true)
+                .song(SongDto.builder()
+                        .lyrics(lyrics)
+                        .title(externalApiSong.getTitle())
+                        .author(externalApiSong.getArtist())
+                        .build())
+                .build();
+        return sessionService.updateSession(sessionDto);
+    }
 
 }
